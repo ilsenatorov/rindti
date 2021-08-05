1d2a815d74195fd56bca57a5161ec3fc4033265c
Ilya Senatorov <il.senatorov@protonmail.com>
2021-08-05 21:01:34 +0200

    updated noisy nodes

--- rindti/models/classification_model.py
+++ rindti/models/classification_model.py
@@ -36,7 +36,6 @@ class ClassificationModel(BaseModel):
 
     def __init__(self, **kwargs):
         super().__init__()
-        print(kwargs)
         self.save_hyperparameters()
         self._determine_feat_method(kwargs["feat_method"], kwargs["drug_hidden_dim"], kwargs["prot_hidden_dim"])
         drug_param = remove_arg_prefix("drug_", kwargs)
