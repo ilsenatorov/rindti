d2fbc55d248ed27efe8a0f0550478340b6303cd6
Ilya Senatorov <il.senatorov@protonmail.com>
2021-08-14 13:05:56 +0200

    added another layer to gmt

--- rindti/layers/graphpool/gmt_net.py
+++ rindti/layers/graphpool/gmt_net.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn.functional as F
 from pytorch_lightning import LightningModule
 from torch import nn
-from torch_geometric.nn import GCNConv, GINConv, GraphSizeNorm, JumpingKnowledge
+from torch_geometric.nn import GCNConv, GINConv
 from torch_geometric.utils import to_dense_batch
 
 from ..base_layer import BaseLayer
@@ -33,15 +33,17 @@ class GMTNet(BaseLayer):
         ratio=0.25,
         max_nodes=600,
         num_heads=4,
-        ln=False,
-        cluster=False,
         **kwargs,
     ):
         super().__init__()
-        num_nodes = ceil(ratio * max_nodes)
-        self.gmpoolg = PMA(input_dim, num_heads, num_nodes, ln=ln, cluster=cluster, mab_conv="GIN")
-        self.sab = SAB(input_dim, output_dim, num_heads, ln=ln, cluster=cluster)
-        self.gmpooli = PMA(output_dim, num_heads, 1, ln=ln, cluster=cluster, mab_conv=None)
+        num_nodes = max_nodes
+        num_nodes = ceil(ratio * num_nodes)
+        self.gmpoolg1 = PMA(input_dim, num_heads, num_nodes, mab_conv="GIN")
+        self.sab1 = SAB(input_dim, hidden_dim, num_heads)
+        num_nodes = ceil(ratio * num_nodes)
+        self.gmpoolg2 = PMA(hidden_dim, num_heads, num_nodes, mab_conv=None)
+        self.sab2 = SAB(hidden_dim, output_dim, num_heads)
+        self.gmpooli = PMA(output_dim, num_heads, 1, mab_conv=None)
 
     def forward(self, x: torch.Tensor, edge_index: torch.Tensor, batch: torch.Tensor, **kwargs):
         """Forward pass"""
@@ -49,12 +51,14 @@ class GMTNet(BaseLayer):
         extended_attention_mask = mask.unsqueeze(1)
         extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)
         extended_attention_mask = (1.0 - extended_attention_mask) * -1e9
-        batch_x = self.gmpoolg(
+        batch_x = self.gmpoolg1(
             batch_x,
             attention_mask=extended_attention_mask,
             graph=(x, edge_index, batch),
         )
-        batch_x = self.sab(batch_x)
+        batch_x = self.sab1(batch_x)
+        batch_x = self.gmpoolg2(batch_x)
+        batch_x = self.sab2(batch_x)
         batch_x = self.gmpooli(batch_x)
         x = batch_x.squeeze(1)
         return x
@@ -63,22 +67,14 @@ class GMTNet(BaseLayer):
 class MAB(LightningModule):
     """Multiset Attention"""
 
-    def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False, cluster=False, conv=None):
+    def __init__(self, dim_Q, dim_K, dim_V, num_heads, conv=None):
         super(MAB, self).__init__()
         self.dim_V = dim_V
         self.num_heads = num_heads
         self.fc_q = nn.Linear(dim_Q, dim_V)
-
         self.fc_k, self.fc_v = self.get_fc_kv(dim_K, dim_V, conv)
-
-        if ln:
-            self.ln0 = nn.LayerNorm(dim_V)
-            self.ln1 = nn.LayerNorm(dim_V)
         self.fc_o = nn.Linear(dim_V, dim_V)
-
         self.softmax_dim = 2
-        if cluster:
-            self.softmax_dim = 1
 
     def forward(self, Q, K, attention_mask=None, graph=None, return_attn=False):
         """Forward pass"""
@@ -105,9 +101,7 @@ class MAB(LightningModule):
             A = torch.softmax(Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), self.softmax_dim)
 
         O = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
-        O = O if getattr(self, "ln0", None) is None else self.ln0(O)
         O = O + F.relu(self.fc_o(O))
-        O = O if getattr(self, "ln1", None) is None else self.ln1(O)
         if return_attn:
             return O, A
         else:
@@ -148,10 +142,10 @@ class MAB(LightningModule):
 class SAB(LightningModule):
     """Self-attention module"""
 
-    def __init__(self, dim_in, dim_out, num_heads, ln=False, cluster=False, mab_conv=None):
+    def __init__(self, dim_in, dim_out, num_heads, mab_conv=None):
         super(SAB, self).__init__()
 
-        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln, cluster=cluster, conv=mab_conv)
+        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, conv=mab_conv)
 
     def forward(self, X, attention_mask=None, graph=None):
         return self.mab(X, X, attention_mask, graph)
@@ -166,16 +160,14 @@ class ISAB(LightningModule):
         dim_out,
         num_heads,
         num_inds,
-        ln=False,
-        cluster=False,
         mab_conv=None,
     ):
         super(ISAB, self).__init__()
         self.I = nn.Parameter(torch.Tensor(1, num_inds, dim_out))
         nn.init.xavier_uniform_(self.I)
 
-        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln, cluster=cluster, conv=mab_conv)
-        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, ln=ln, cluster=cluster, conv=mab_conv)
+        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, conv=mab_conv)
+        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, conv=mab_conv)
 
     def forward(self, X, attention_mask=None, graph=None):
         """Forward pass"""
@@ -186,12 +178,12 @@ class ISAB(LightningModule):
 class PMA(LightningModule):
     """Need to figure this out"""
 
-    def __init__(self, dim, num_heads, num_seeds, ln=False, cluster=False, mab_conv=None):
+    def __init__(self, dim, num_heads, num_seeds, mab_conv=None):
         super(PMA, self).__init__()
         self.S = nn.Parameter(torch.Tensor(1, num_seeds, dim))
         nn.init.xavier_uniform_(self.S)
 
-        self.mab = MAB(dim, dim, dim, num_heads, ln=ln, cluster=cluster, conv=mab_conv)
+        self.mab = MAB(dim, dim, dim, num_heads, conv=mab_conv)
 
     def forward(self, X, attention_mask=None, graph=None, return_attn=False):
         """Forward pass"""
