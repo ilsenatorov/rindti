25f09c316bd5b3404c32d2472d909274bffd549c
Ilya Senatorov <il.senatorov@protonmail.com>
2021-08-31 13:41:56 +0200

    added plddt structure parsing

--- workflow/scripts/create_pymol_scripts.py
+++ workflow/scripts/create_pymol_scripts.py
@@ -1,43 +1,56 @@
 import os
 
-config = snakemake.config
 
-script = """
+def create_script(protein, config):
+    """
+    Create pymol parsing script for a protein according to the config
+    """
+    fmt_keywords = {"protein": protein}
+    script = """
 import psico.fullinit
 from glob import glob
 
 cmd.load("resources/structures/{protein}.pdb")
-lst = glob("resources/templates/*.pdb")
-templates = [x.split('/')[-1].split('.')[0] for x in lst]
-for i in lst:cmd.load(i)
-scores = {{x : cmd.tmalign("{protein}", x) for x in templates}}
-max_score = max(scores, key=scores.get)
-
-cmd.extra_fit("name CA", max_score, "tmalign")
 """
 
-if config["structures"] == "bsite":
-    radius = config["bsite"]["radius"]
+    if config["structures"] == "plddt":
+        script += """
+            cmd.select("result", "b > {threshold}")
+            """
+        fmt_keywords["threshold"] = config["plddt"]["threshold"]
+    else:
+        # template-based
+        assert os.path.isdir("resources/templates")
+        script += """
+            lst = glob("resources/templates/*.pdb")
+            templates = [x.split('/')[-1].split('.')[0] for x in lst]
+            for i in lst:cmd.load(i)
+            scores = {{x : cmd.tmalign("{protein}", x) for x in templates}}
+            max_score = max(scores, key=scores.get)
+
+            cmd.extra_fit("name CA", max_score, "tmalign")
+            """
+
+        if config["structures"] == "bsite":
+            fmt_keywords["radius"] = config["bsite"]["radius"]
+            script += """
+                cmd.select("result", "br. {protein} within {radius} of organic")
+                """
+        elif config["structures"] == "template":
+            fmt_keywords["radius"] = config["template"]["radius"]
+            script += """
+                cmd.select("result", "br. {protein} within {radius} of not {protein} and name CA")
+                """
     script += """
-cmd.select("bsite", "br. {protein} within {radius} of organic")
-cmd.save("results/parsed_structures_bsite/{protein}.pdb", "bsite")
-"""
-elif config["structures"] == "template":
-    radius = config["template"]["radius"]
-    script += """
-select template, br. {protein} within {radius} of not {protein} and name CA
-save results/parsed_structures_template/{protein}.pdb, template
-"""
-else:
-    raise ValueError("Unknown structures type!")
+        cmd.save("results/parsed_structures_{structures}/{protein}.pdb", "result")"""
+    fmt_keywords["structures"] = config["structures"]
+
+    return script.format(**fmt_keywords)
 
 
-for output in snakemake.output:
-    protein = os.path.basename(output).split(".")[0]
-    with open(output, "w") as file:
-        file.write(
-            script.format(
-                protein=protein,
-                radius=radius,
-            )
-        )
+if __name__ == "__main__":
+    for output in snakemake.output:
+        protein = os.path.basename(output).split(".")[0]
+
+        with open(output, "w") as file:
+            file.write(create_script(protein, snakemake.config))
