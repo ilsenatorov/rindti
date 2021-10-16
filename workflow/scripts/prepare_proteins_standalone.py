1c8fb50e1cfbf99923226117f53e5526e47b9689
Ilya Senatorov <il.senatorov@protonmail.com>
2021-10-16 14:05:18 +0200

    checks file presence in standalone

--- workflow/scripts/prepare_proteins_standalone.py
+++ workflow/scripts/prepare_proteins_standalone.py
@@ -15,11 +15,17 @@ args = parser.parse_args()
 
 for node in ["label", "onehot"]:
     for edge in ["label", "onehot", "none"]:
-        proteins = pd.Series(list(os.listdir(args.folder)), name="ID")
-        proteins = pd.DataFrame(proteins)
-        proteins["sif"] = proteins["ID"].apply(lambda x: os.path.join(args.folder, x, x + "_h.sif"))
-        proteins.set_index("ID", inplace=True)
-        prot_encoder = ProteinEncoder(node, edge)
-        data = Parallel(n_jobs=args.j)(delayed(prot_encoder)(i) for i in tqdm(proteins["sif"]))
-        proteins["data"] = data
-        proteins.to_pickle(os.path.join(args.outdir, "pfam_{}_{}.pkl".format(node, edge)))
+        filename = "pfam_{}_{}.pkl".format(node, edge)
+        print("working on " + filename)
+        if filename not in os.listdir(args.outdir):
+            print("Processing!")
+            proteins = pd.Series(list(os.listdir(args.folder)), name="ID")
+            proteins = pd.DataFrame(proteins)
+            proteins["sif"] = proteins["ID"].apply(lambda x: os.path.join(args.folder, x, x + "_h.sif"))
+            proteins.set_index("ID", inplace=True)
+            prot_encoder = ProteinEncoder(node, edge)
+            data = Parallel(n_jobs=args.j)(delayed(prot_encoder)(i) for i in tqdm(proteins["sif"]))
+            proteins["data"] = data
+            proteins.to_pickle(os.path.join(args.outdir, filename))
+        else:
+            print("It's present!")
