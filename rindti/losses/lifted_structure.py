8a4da6593821edfd6c71330628281593b12a266d
Ilya Senatorov <il.senatorov@protonmail.com>
2021-11-10 11:41:43 +0100

    fixed lifted structure loss

--- rindti/losses/lifted_structure.py
+++ rindti/losses/lifted_structure.py
@@ -8,23 +8,17 @@ from torch import Tensor
 class GeneralisedLiftedStructureLoss(LightningModule):
     """https://arxiv.org/abs/1511.06452"""
 
-    def __init__(self, pos_margin: int, neg_margin: int) -> None:
+    def __init__(self, pos_margin: int = 0, neg_margin: int = 1) -> None:
         super().__init__()
         self.pos_margin = pos_margin
         self.neg_margin = neg_margin
 
-    def forward(self, embeds: Tensor, fam_idx: List[List[int]]) -> Tensor:
+    def forward(self, embeds: Tensor, fam_idx: List[int]) -> Tensor:
         """Calculate the loss"""
-        batch_size = embeds.size(0)
-        losses = []
         dist = torch.cdist(embeds, embeds)
-        all_idx = set(range(batch_size))
-        for idx in fam_idx:
-            pos_idxt = torch.tensor(idx)
-            neg_idxt = torch.tensor(list(all_idx.difference(idx)))
-            pos = dist[pos_idxt[:, None], pos_idxt]
-            neg = dist[neg_idxt[:, None], pos_idxt]
-            pos_loss = torch.logsumexp(pos - self.pos_margin, dim=0)
-            neg_loss = torch.logsumexp(self.neg_margin - neg, dim=0)
-            losses.append(torch.relu(pos_loss + neg_loss) ** 2)
-        return torch.cat(losses).view(-1)
+        fam_mask = (fam_idx == fam_idx.t()).float()
+        pos = (dist - self.pos_margin) * fam_mask
+        neg = (self.neg_margin - dist) * (1 - fam_mask)
+        pos_loss = torch.logsumexp(pos, dim=0)
+        neg_loss = torch.logsumexp(neg, dim=0)
+        return torch.relu(pos_loss + neg_loss)
