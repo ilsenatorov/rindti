a2c5d31141fe6cb5d8b77c58b7c7786dc36b6a45
Ilya Senatorov <il.senatorov@protonmail.com>
2021-11-12 12:07:14 +0100

    updated embedding dash

--- dash/embedding_vis.py
+++ dash/embedding_vis.py
@@ -1,17 +1,30 @@
+import json
+
+import dash_bio as dashbio
+import dash_bio_utils.ngl_parser as ngl_parser
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 app = dash.Dash(__name__)
+
+df = pd.read_csv("data/embeddings.tsv", sep="\t")
+df["select"] = "grey"
 
 
 app.layout = html.Div(
     style={"display": "flex", "flex-direction": "row"},
     children=[
+        html.Div(
+            [
+                dashbio.NglMoleculeViewer(id="molecule"),
+                html.A("UniProt Link", id="uniprot_link"),
+            ]
+        ),
         html.Div([dcc.Graph(id="fig1")]),
         html.Div(
             [
@@ -37,8 +50,40 @@ app.layout = html.Div(
     ],
 )
 
-df = pd.read_csv("data/embeddings.tsv", sep="\t").iloc[:1000]
-df["select"] = "grey"
+
+@app.callback(Output("uniprot_link", "href"), Input("fig1", "clickData"))
+def get_uniprot_link(clickData):
+    """show link to uniprot"""
+    if clickData is None:
+        raise PreventUpdate
+    prot_id, fams = clickData["points"][0]["customdata"]
+    return f"https://www.uniprot.org/uniprot/{prot_id}"
+
+
+@app.callback(Output("molecule", "data"), Output("molecule", "molStyles"), Input("fig1", "clickData"))
+def return_molecule(prot_dict):
+    """Get molecular visualisation on click"""
+    if prot_dict is None:
+        raise PreventUpdate
+    prot_id = prot_dict["points"][0]["customdata"][0]
+
+    molstyles_dict = {
+        "representations": ["cartoon"],
+        "chosenAtomsColor": "white",
+        "chosenAtomsRadius": 1,
+        "molSpacingXaxis": 100,
+    }
+
+    data_list = [
+        ngl_parser.get_data(
+            data_path="https://alphafold.ebi.ac.uk/files/",
+            pdb_id=f"AF-{prot_id}-F1-model_v1",
+            color="blue",
+            reset_view=True,
+            local=False,
+        )
+    ]
+    return data_list, molstyles_dict
 
 
 @app.callback(
@@ -56,8 +101,7 @@ def output_fig(opacity, fam_search, prot_search, size):
     if fam_search:
         data.loc[data["fam"].str.contains(fam_search), "select"] = "red"
     if prot_search:
-        data.loc[data["id"].str.contains(prot_search), "select"] = "red"
-    print(data)
+        data.loc[data["id"].str.contains(prot_search), "select"] = "blue"
     fig = go.Figure(
         data=[
             go.Scattergl(
@@ -74,8 +118,8 @@ def output_fig(opacity, fam_search, prot_search, size):
         title="Embeddings",
         xaxis_title="x",
         yaxis_title="y",
-        width=1000,
-        height=1000,
+        width=800,
+        height=800,
         clickmode="event+select",
     )
     return fig
