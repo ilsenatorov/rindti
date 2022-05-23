47fc71874c3460704ae15b2ee6ac08d8603b222d
Ilya Senatorov <il.senatorov@protonmail.com>
2022-05-23 23:33:48 +0200

    improved code reusability

--- test/conftest.py
+++ test/conftest.py
@@ -5,38 +5,47 @@ import subprocess
 import pytest
 from pytorch_lightning.utilities.seed import seed_everything
 
-from rindti.data import DTIDataModule
+from rindti.data import DTIDataModule, PreTrainDataModule
 
 
-def run_snakemake(source: str):
+def run_snakemake(*args):
     """Run snakemake with the given source dir."""
     subprocess.run(
-        [
-            "snakemake",
-            "-j",
-            "4",
-            "--forceall",
-            "--use-conda",
-            "--config",
-            f"source={source}",
-        ],
+        ["snakemake", "-j", "4", "--forceall", "--use-conda", "--config", *args],
         check=True,
     )
 
 
 @pytest.fixture(scope="session")
-def dti_pickle(tmpdir_factory) -> str:
+def snakemake_dir(tmpdir_factory):
     tmpdir = tmpdir_factory.mktemp("test_data")
     newdir = shutil.copytree("test/test_data/resources", str(tmpdir.join("resources")))
-    run_snakemake(newdir)
-    result = os.listdir(tmpdir.join("results/prepare_all"))[0]
-    return tmpdir.join("results/prepare_all", result)
+    run_snakemake(f"source={newdir}")
+    return tmpdir
+
+
+@pytest.fixture(scope="session")
+def dti_pickle(snakemake_dir) -> str:
+    result = os.listdir(snakemake_dir.join("results/prepare_all"))[0]
+    return snakemake_dir.join("results/prepare_all", result)
+
+
+@pytest.fixture(scope="session")
+def pretrain_pickle(snakemake_dir) -> str:
+    result = os.listdir(snakemake_dir.join("results/prot_data"))[0]
+    return snakemake_dir.join("results/prot_data", result)
 
 
 @pytest.fixture()
 def dti_datamodule(dti_pickle: str):
     """Run snakemake with the given config."""
     return DTIDataModule(dti_pickle, "test", batch_size=4)
+
+
+@pytest.fixture()
+def pretrain_datamodule(pretrain_pickle: str):
+    """Run snakemake with the given config."""
+    return PreTrainDataModule(pretrain_pickle, "test", batch_size=4)
 
 
 @pytest.fixture(autouse=True)
