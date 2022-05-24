dfcb4bc1b570a4a624d2f49bb45387455aecfe3d
Old-Shatterhand <70888826+Old-Shatterhand@users.noreply.github.com>
2022-05-24 17:05:19 +0200

    Removed unnecessary prints

--- workflow/scripts/prepare_all.py
+++ workflow/scripts/prepare_all.py
@@ -51,18 +51,15 @@ if __name__ == "__main__":
     with open(snakemake.input.prots, "rb") as file:
         prots = pickle.load(file)
 
-    print(interactions)
-    print(prots)
-    print(drugs)
-    print("====================================")
     interactions = interactions[interactions["Target_ID"].isin(prots.index)]
-    print(interactions.shape)
     interactions = interactions[interactions["Drug_ID"].isin(drugs.index)]
-    print(interactions.shape)
+    
     prots = prots[prots.index.isin(interactions["Target_ID"].unique())]
     drugs = drugs[drugs.index.isin(interactions["Drug_ID"].unique())]
+    
     prot_count = interactions["Target_ID"].value_counts()
     drug_count = interactions["Drug_ID"].value_counts()
+    
     prots["data"] = prots.apply(lambda x: {**x["data"], "count": prot_count[x.name]}, axis=1)
     drugs["data"] = drugs.apply(lambda x: {**x["data"], "count": drug_count[x.name]}, axis=1)
 
