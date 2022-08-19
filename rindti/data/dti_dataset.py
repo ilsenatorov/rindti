8280830cb6a819eaa10237c42432fa5c3c3a8a8d
Ilya Senatorov <il.senatorov@protonmail.com>
2022-08-19 16:18:34 +0200

    moved pdb_parser to a separate file for datasets

--- rindti/data/dti_dataset.py
+++ rindti/data/dti_dataset.py
@@ -51,7 +51,6 @@ class DTIDataset(InMemoryDataset):
 
     def _get_datum(self, all_data: dict, id: str, which: str, **kwargs) -> dict:
         """Get either prot or drug data."""
-        # MEME comment to see if test difference works
         graph = all_data[which].loc[id, "data"]
         graph["id"] = id
         if (
@@ -86,51 +85,3 @@ class DTIDataset(InMemoryDataset):
                     data_list.append(two_graph_data)
                 if data_list:
                     self.process_(data_list, split)
-
-
-class PreTrainDataset(InMemoryDataset):
-    """Dataset class for pre-training.
-
-    Args:
-        filename (str): Pickle file that stores the data
-        split (str, optional): Split type ('train', 'val', 'test). Defaults to "train".
-        transform (Callable, optional): transformer to apply on each access. Defaults to None.
-        pre_transform (Callable, optional): pre-transformer to apply once before. Defaults to None.
-    """
-
-    def __init__(self, filename: str, transform: Callable = None, pre_transform: Callable = None):
-        basefilename = os.path.basename(filename)
-        basefilename = os.path.splitext(basefilename)[0]
-        root = os.path.join("data", basefilename)
-        self.filename = filename
-        super().__init__(root, transform, pre_transform)
-        self.data, self.slices, self.config = torch.load(self.processed_paths[0])
-
-    def index(self, id: str):
-        """Find protein by id."""
-        return self[self.data.id.index(id)]
-
-    @property
-    def processed_file_names(self) -> Iterable[str]:
-        """Which files have to be in the dir to consider dataset processed.
-
-        Returns:
-            Iterable[str]: list of files
-        """
-        return ["data.pt"]
-
-    def process(self):
-        """If the dataset was not seen before, process everything."""
-        data = pd.read_pickle(self.filename)
-        data_list = []
-        for i in data["data"]:
-            data_list.append(Data(**i))
-
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-        self.config = data["config"]
-        data, slices = self.collate(data_list)
-        torch.save((data, slices, self.config), self.processed_paths[0])
---
+++ rindti/data/pdb_parser.py
@@ -0,0 +1,81 @@
+import torch
+
+node_encode = {
+    "ala": 0,
+    "arg": 1,
+    "asn": 2,
+    "asp": 3,
+    "cys": 4,
+    "gln": 5,
+    "glu": 6,
+    "gly": 7,
+    "his": 8,
+    "ile": 9,
+    "leu": 10,
+    "lys": 11,
+    "met": 12,
+    "phe": 13,
+    "pro": 14,
+    "ser": 15,
+    "thr": 16,
+    "trp": 17,
+    "tyr": 18,
+    "val": 19,
+}
+
+
+class Residue:
+    """Residue class"""
+
+    def __init__(self, line: str) -> None:
+        self.name = line[17:20].strip()
+        self.num = int(line[22:26].strip())
+        self.chainID = line[21].strip()
+        self.x = float(line[30:38].strip())
+        self.y = float(line[38:46].strip())
+        self.z = float(line[46:54].strip())
+        self.plddt = float(line[60:66].strip())
+
+
+class PDBStructure:
+    """Structure class"""
+
+    def __init__(self, filename: str) -> None:
+        self.residues = {}
+        self.parse_file(filename)
+
+    def parse_file(self, filename: str) -> None:
+        """Parse PDB file"""
+        for line in open(filename, "r"):
+            if line.startswith("ATOM") and line[12:16].strip() == "CA":
+                res = Residue(line)
+                self.residues[res.num] = res
+
+    def get_coords(self) -> torch.Tensor:
+        """Get coordinates of all atoms"""
+        coords = [[res.x, res.y, res.z] for res in self.residues.values()]
+        return torch.tensor(coords)
+
+    def get_nodes(self) -> torch.Tensor:
+        """Get features of all nodes of a graph"""
+        return torch.tensor([node_encode[res.name.lower()] for res in self.residues.values()])
+
+    def get_edges(self, threshold: float) -> torch.Tensor:
+        """Get edges of a graph using threshold as a cutoff"""
+        coords = self.get_coords()
+        dist = torch.cdist(coords, coords)
+        edges = torch.where(dist < threshold)
+        edges = torch.cat([arr.view(-1, 1) for arr in edges], axis=1)
+        edges = edges[edges[:, 0] != edges[:, 1]]
+        return edges.t()
+
+    def get_graph(self) -> dict:
+        """Get a graph using threshold as a cutoff"""
+        nodes = []
+        pos = []
+        plddt = []
+        for res in self.residues.values():
+            nodes.append(node_encode[res.name.lower()])
+            pos.append([res.x, res.y, res.z])
+            plddt.append(res.plddt)
+        return dict(x=torch.tensor(nodes), pos=torch.tensor(pos), plddt=torch.tensor(plddt))
