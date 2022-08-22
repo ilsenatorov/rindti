55356a9b39f3cd0f1faa0590315064acd75867af
Roman Joeres <Roman.Joeres@helmholtz-hzi.de>
2022-08-22 14:39:44 +0200

    Another bug fix in testing

--- test/test_snakemake.py
+++ test/test_snakemake.py
@@ -18,58 +18,58 @@ class TestSnakeMake:
     """Runs all snakemake tests."""
 
     @pytest.mark.parametrize("config_file", snakemake_configs)
-    def test_configs(self, snakemake_config: dict, config_file: dict):
+    def test_configs(self, dti_snakemake_config: dict, config_file: dict):
         """Test all snakemake configs."""
-        snakemake_config.update(read_config(config_file))
-        validate(snakemake_config, "workflow/schemas/config.schema.yaml")
+        dti_snakemake_config.update(read_config(config_file))
+        validate(dti_snakemake_config, "workflow/schemas/config.schema.yaml")
 
     @pytest.mark.parametrize("method", ["whole", "plddt", "bsite", "template"])
-    def test_structures(self, method: str, snakemake_config: dict, tmpdir_factory: str):
+    def test_structures(self, method: str, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the various structure-parsing methods."""
-        snakemake_config["prots"]["structs"]["method"] = method
-        snakemake_config["only_prots"] = True
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["prots"]["structs"]["method"] = method
+        dti_snakemake_config["only_prots"] = True
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
 
     @pytest.mark.gpu
-    def test_features_esm(self, snakemake_config: dict, tmpdir_factory: str):
+    def test_features_esm(self, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the graph creation methods."""
-        snakemake_config["prots"]["features"]["method"] = "esm"
-        snakemake_config["only_prots"] = True
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["prots"]["features"]["method"] = "esm"
+        dti_snakemake_config["only_prots"] = True
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
 
     @pytest.mark.parametrize("features", ["rinerator", "distance"])  # NOTE esm testing disabled
-    def test_features(self, features: str, snakemake_config: dict, tmpdir_factory: str):
+    def test_features(self, features: str, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the graph creation methods."""
-        snakemake_config["prots"]["features"]["method"] = features
-        snakemake_config["only_prots"] = True
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["prots"]["features"]["method"] = features
+        dti_snakemake_config["only_prots"] = True
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
 
     @pytest.mark.parametrize("node_feats", ["label", "onehot"])
     @pytest.mark.parametrize("edge_feats", ["label", "onehot", "none"])
-    def test_prot_encodings(self, node_feats: str, edge_feats: str, snakemake_config: dict, tmpdir_factory: str):
+    def test_prot_encodings(self, node_feats: str, edge_feats: str, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the encoding methods for prot nodes and edges."""
-        snakemake_config["prots"]["features"]["node_feats"] = node_feats
-        snakemake_config["prots"]["features"]["edge_feats"] = edge_feats
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["prots"]["features"]["node_feats"] = node_feats
+        dti_snakemake_config["prots"]["features"]["edge_feats"] = edge_feats
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
 
     @pytest.mark.parametrize("node_feats", ["label", "onehot", "glycan"])
     @pytest.mark.parametrize("edge_feats", ["label", "onehot", "none"])
-    def test_drug_encodings(self, node_feats: str, edge_feats: str, snakemake_config: dict, tmpdir_factory: str):
+    def test_drug_encodings(self, node_feats: str, edge_feats: str, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the encoding methods for drug nodes and edges."""
-        snakemake_config["drugs"]["node_feats"] = node_feats
-        snakemake_config["drugs"]["edge_feats"] = edge_feats
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["drugs"]["node_feats"] = node_feats
+        dti_snakemake_config["drugs"]["edge_feats"] = edge_feats
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
 
     @pytest.mark.parametrize("split", ["random", "drug", "target"])
-    def test_splits(self, split: str, snakemake_config: dict, tmpdir_factory: str):
+    def test_splits(self, split: str, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the dataset splitting methods."""
-        snakemake_config["split_data"]["method"] = split
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["split_data"]["method"] = split
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
 
     @pytest.mark.parametrize("filtering", ["all", "posneg"])
     @pytest.mark.parametrize("sampling", ["none", "over", "under"])
-    def test_parse_dataset(self, filtering: str, sampling: str, snakemake_config: dict, tmpdir_factory: str):
+    def test_parse_dataset(self, filtering: str, sampling: str, dti_snakemake_config: dict, tmpdir_factory: str):
         """Test the dataset filtering and sampling methods."""
-        snakemake_config["parse_dataset"]["filtering"] = filtering
-        snakemake_config["parse_dataset"]["sampling"] = sampling
-        run_snakemake(snakemake_config, tmpdir_factory)
+        dti_snakemake_config["parse_dataset"]["filtering"] = filtering
+        dti_snakemake_config["parse_dataset"]["sampling"] = sampling
+        run_snakemake(dti_snakemake_config, tmpdir_factory)
